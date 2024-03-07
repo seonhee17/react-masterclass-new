27bba30c82a8ec7de03a8a96636d0c9cb944c630
LEE SEONHEE <leeseonhee17@gmail.com>
2024-03-07 13:52:26 +0900

    #4.3 강의 까지

--- src/Router.tsx
+++ src/Router.tsx
@@ -10,11 +10,11 @@ const Router = createBrowserRouter([
         element : <Root />,
         children : [
             {
-                path : "",
+                path : "/",
                 element:<Home />,
             },
             {
-                path : "about",
+                path : "/about",
                 element:<About />,
             },
         ],
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { RouterProvider } from "react-router-dom";
+import Router from "./Router";
 
 
 
@@ -9,6 +10,6 @@ const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
 root.render(
   <React.StrictMode>
-      <App />
+      <RouterProvider router={Router} />
   </React.StrictMode>,
 );
